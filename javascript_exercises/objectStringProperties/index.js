56289e8edea591b40ead795bb97f22f6a45aba51
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-11-26 21:54:51 +0530

    changed html file and added js file

---
+++ javascript_exercises/objectStringProperties/index.js
@@ -0,0 +1,7 @@
+let obj={};
+obj.name=prompt("enter a name: ");
+obj.age=prompt("enter an age in numbers: ");
+obj.height=prompt("enter height: ");
+obj.country=prompt("enter your country: ")
+obj.designation=prompt("enter your designation: ");
+console.log(obj);
