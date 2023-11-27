cc145f677bd51a9a5a4687fb66d92c1e41b593ae
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-11-27 10:25:24 +0530

    finished js file

--- javascript_exercises/objectStringProperties/index.js
+++ javascript_exercises/objectStringProperties/index.js
@@ -1,7 +1,14 @@
-let obj={};
-obj.name=prompt("enter a name: ");
-obj.age=prompt("enter an age in numbers: ");
-obj.height=prompt("enter height: ");
-obj.country=prompt("enter your country: ")
-obj.designation=prompt("enter your designation: ");
-console.log(obj);
+let obj = {};
+obj.name = prompt("enter a name: ");
+obj.age = parseInt(prompt("enter an age in numbers: "));
+obj.height = parseInt(prompt("enter height: "));
+obj.country = prompt("enter your country: ");
+obj.designation = prompt("enter your designation: ");
+function stringProp(curObj) {
+  for (let key in curObj) {
+    if (typeof curObj[key] === "number") {
+      console.log(key, ":", curObj[key]);
+    }
+  }
+}
+stringProp(obj);
